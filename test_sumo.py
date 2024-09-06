b28b082d88c4f3e9bca806eccac343c39701f9ea
alpc91 <459388027@qq.com>
2024-09-06 10:50:52 +0800

    dev_spider_test

--- test_sumo.py
+++ test_sumo.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 
-for ckpt in range(1, 189):
-    command = f"python display.py --cfg config/test-robo-sumo-sgdevant-devant-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models"
+for ckpt in range(1, 200):
+    command = f"python display.py --cfg config/test-robo-sumo-sgdevspider-devspider-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models"
     os.system(command)
