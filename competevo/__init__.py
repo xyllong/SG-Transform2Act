1cbc09fcdfee2fb2ec1f4acbeb16457fc23ce435
alpc91 <459388027@qq.com>
2024-09-07 14:26:54 +0800

    evo_ant_fighter

--- competevo/__init__.py
+++ competevo/__init__.py
@@ -24,7 +24,7 @@ register(
     disable_env_checker=True,
     kwargs={'agent_names': ['evo_ant_fighter', 'evo_ant_fighter'],
             'world_xml_path': "./competevo/evo_envs/assets/world_body_arena.xml",
-            'init_pos': [(-1, 0, 1.5), (1, 0, 1.5)],
+            'init_pos': [(-1, 0, 1.3), (1, 0, 1.3)],
             'ini_euler': [(0, 0, 0), (0, 0, 180)],
             # 'rgb': [(0.98, 0.54, 0.56), (0.11, 0.56, 1)],
             'rgb': [(0.98, 0.87, 0.67), (0.98, 0.87, 0.67)],
--- competevo/evo_envs/robo_sumo_evo.py
+++ competevo/evo_envs/robo_sumo_evo.py
@@ -182,7 +182,7 @@ class RoboSumoEvoEnv(MultiEvoAgentEnv):
             self_xyz = agent.get_qpos()[:3]
             # Loose penalty
             infos[i]['lose_penalty'] = 0.
-            if (self_xyz[2] < 0.29 + self.arena_height or 
+            if (self_xyz[2] < 0.29 + self.arena_height or self_xyz[2] > 1.1 + self.arena_height or 
                 np.sqrt(np.sum(self_xyz[:2]**2)) > self.RADIUS):
                 infos[i]['lose_penalty'] = - self.WIN_REWARD
                 dones[i] = True
@@ -192,7 +192,7 @@ class RoboSumoEvoEnv(MultiEvoAgentEnv):
             for j, opp in self.agents.items():
                 if i == j: continue
                 opp_xyz = opp.get_qpos()[:3]
-                if (opp_xyz[2] < 0.29 + self.arena_height or 
+                if (opp_xyz[2] < 0.29 + self.arena_height or opp_xyz[2] > 1.1 + self.arena_height or 
                     np.sqrt(np.sum(opp_xyz[:2]**2)) > self.RADIUS):
                     infos[i]['win_reward'] += self.WIN_REWARD
                     infos[i]['winner'] = True
