0f52ad9354f63734147fb25182bcfba86a2e11b8
xyllong <xiayl23@mails.tsinghua.edu.cn>
2024-09-21 15:00:57 +0000

    navi-ant

--- test_sumo.py
+++ test_sumo.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 
-for ckpt in range(1, 200):
+for ckpt in range(1, 400):
     command = f"python display.py --cfg config/SG/robo-sumo-sgant-ant-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models"
     os.system(command)
